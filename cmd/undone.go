7d9a3f8e9251f0baaf60e06e51c9719b5057a6b3
Ken Hibino <ken.hibino7@gmail.com>
2019-03-26 07:02:34 -0700

    Add undone command

---
+++ cmd/undone.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(undoneCmd)
+}
+
+var undoneCmd = &cobra.Command{
+	Use:   "undone [id]",
+	Short: "Mark a task to be undone",
+	Args:  cobra.MinimumNArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		todos, err := readFromFile(filename)
+		if err != nil {
+			log.Fatalf("todos undone %s: %v\n", args[0], err)
+		}
+		for i, todo := range todos {
+			if todo.ID == args[0] {
+				todos[i].Done = false
+			}
+		}
+		if err := writeToFile(todos, filename); err != nil {
+			log.Fatalf("todos undone %s: %v\n", args[0], err)
+		}
+	},
+}
