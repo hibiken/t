64a2b248036c767588a28cc829a7b6cffba0aa0d
Ken Hibino <ken.hibino7@gmail.com>
2019-03-26 18:49:22 -0700

    Add prune command

---
+++ cmd/prune.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var pruneCmd = &cobra.Command{
+	Use:   "prune",
+	Short: "Deletes all done todo items",
+	Args:  cobra.NoArgs,
+	Run: func(_ *cobra.Command, _ []string) {
+		todos, err := readFromFile(filename)
+		if err != nil {
+			log.Fatalf("todos prune: %v\n", err)
+		}
+		var res []*Todo
+		for _, t := range todos {
+			if !t.Done {
+				res = append(res, t)
+			}
+		}
+		if err := writeToFile(res, filename); err != nil {
+			log.Fatalf("todos prune: %v\n", err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(pruneCmd)
+}
