7742e3e09df5c8b2b01dda85cf2666e744513e23
Ken Hibino <ken.hibino7@gmail.com>
2019-03-27 16:13:26 -0700

    Print a different message if there are no todos

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -39,6 +39,10 @@ func readFromFile(filename string) ([]*Todo, error) {
 // printTodos prints todos. If all is false, it prints only the items with Done
 // field set to false, otherwise it prints all items in the slice.
 func printTodos(todos []*Todo, all bool) {
+	if len(todos) == 0 {
+		fmt.Println("You'are all done 🎉")
+		return
+	}
 	table := tablewriter.NewWriter(os.Stdout)
 	table.SetHeader([]string{"ID", "Title", "Status"})
 	table.SetBorder(false)
