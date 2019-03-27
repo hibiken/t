c638353638915c380904e1e5d8788d376a6aa434
Ken Hibino <ken.hibino7@gmail.com>
2019-03-26 19:01:26 -0700

    Add clearall command

---
+++ cmd/clearall.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+var clearallCmd = &cobra.Command{
+	Use:   "clearall",
+	Short: "Deletes all todos",
+	Args:  cobra.NoArgs,
+	Run: func(_ *cobra.Command, _ []string) {
+		if err := writeToFile([]*Todo{}, filename); err != nil {
+			log.Fatalf("todos clearall: %v\n", err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(clearallCmd)
+}
